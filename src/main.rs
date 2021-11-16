a4476591bf3275116cf9fc814d61cf72c1605c66
NekoMio <hui@nekomio.com>
2021-11-16 18:09:27 +0800

    完成了文法的读入

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,93 @@
+// 语法分析
+
+mod read;
+
+use read::*;
+
+/// LL1 语法分析器
+/// ```
+/// let result_ll = run_ll1(str);
+/// ```
+/// 返回运行循环次数，或者错误
+fn run_ll1(input: &str) -> Result<u32, String> {
+    return Ok(0);
+}
+
+/// LR1 语法分析器
+/// 
+/// ```
+/// let result_lr = run_lr1(str);
+/// ```
+/// 返回运行循环次数，或者错误
+fn run_lr1(input: &str) -> Result<u32, String> {
+    return Ok(0);
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    let argv: usize = args.len();
+    let contents : String;
+    let grammar : Grammar;
+
+    match argv {
+        1 => {
+            grammar = readgrammar();
+            contents = readcontent();
+        },
+        2 => {
+            let filename = &args[1];
+            contents = readfile(filename);
+            grammar = readgrammar();
+        },
+        3 => {
+            let filename = &args[1];
+            let grammar_filename = &args[2];
+            contents = readfile(filename);
+            grammar = readgrammarfile(grammar_filename);
+        }
+        _ => {
+            println!("Usage {}", args[0]);
+            println!("Usage {} <filename>", args[0]);
+            println!("Usage {} <filename> <grammarfile>", args[0]);
+            return;
+        }
+    }
+    println!("{:?}", grammar);
+    println!("{}", contents);
+    let result_ll = run_ll1(&contents);
+    if let Err(error) = result_ll {
+        println!("{}", error);
+    }
+    let result_lr = run_lr1(&contents);
+    if let Err(error) = result_lr {
+        println!("{}", error);
+    }
+    
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    /// 测试文件读取
+    fn test_readfile() {
+        let contents = readfile("test.txt");
+        assert_eq!(contents, "hello world");
+    }
+    #[test]
+    fn test_readgrammarfile() {
+        let grammar = readgrammarfile("testgrammar.txt");
+        assert_eq!(grammar.nonterminals, vec!["S".to_string(), "A".to_string(), "B".to_string()]);
+        assert_eq!(grammar.terminals, vec!["a".to_string(), "b".to_string()]);
+        assert_eq!(grammar.start, "S".to_string());
+        assert_eq!(grammar.rules, vec![
+            ("S".to_string(), vec!["A".to_string(), "B".to_string()]),
+            ("A".to_string(), vec!["a".to_string(), "B".to_string()]),
+            ("B".to_string(), vec!["b".to_string(), "A".to_string()]),
+        ]);
+    }
 }
---
+++ src/read.rs
@@ -0,0 +1,152 @@
+use std::fs::File;
+use std::io::Read;
+use std::io::Write;
+
+/// 语法结构体
+///
+/// @terminals: 终结符集合  
+///
+/// @nonterminals: 非终结符集合  
+///
+/// @rules: 产生式集合  
+///
+/// @start: 开始符 
+#[derive(Debug)]
+pub struct Grammar {
+    pub terminals: Vec<String>,
+    pub nonterminals: Vec<String>,
+    pub start: String,
+    pub rules: Vec<(String, Vec<String>)>,
+}
+
+/// 读取文件
+/// ```
+/// let contents = readfile("test.txt");
+/// ```
+pub fn readfile(filename: &str) -> String {
+    let mut f = File::open(filename).expect("file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+    return contents;
+}
+
+/// 从命令行读取内容
+pub fn readcontent() -> String {
+    let mut content = String::new();
+    std::io::stdin()
+        .read_line(&mut content)
+        .expect("read error");
+    return content;
+}
+
+/// 从命令行读取文法
+pub fn readgrammar() -> Grammar {
+    let mut input = String::new();
+    print!("请输入文法非终结符个数：");
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut input).expect("read error");
+    let m = input.trim().parse::<u32>().expect("请输入整数");
+    input.clear();
+    print!("请输入文法非终结符：");
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut input).expect("read error");
+    let mut nonterminal = input.trim().split_whitespace();
+    let mut nonterminal_vec = Vec::new();
+    for _ in 0..m {
+        nonterminal_vec.push(nonterminal.next().unwrap().to_string());
+    }
+    input.clear();
+    print!("请输入文法终结符个数：");
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut input).expect("read error");
+    // std::io::stderr().write(input.to_string().as_bytes()).unwrap();
+    let n = input.trim().parse::<u32>().expect("请输入整数");
+    input.clear();
+    print!("请输入文法终结符：");
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut input).expect("read error");
+    let mut terminal = input.trim().split_whitespace();
+    let mut terminal_vec = Vec::new();
+    for _ in 0..n {
+        terminal_vec.push(terminal.next().unwrap().to_string());
+    }
+    input.clear();
+    print!("请输入文法开始符：");
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut input).expect("read error");
+    let start = input.trim().to_string();
+    input.clear();
+    print!("请输入文法产生式个数：");
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut input).expect("read error");
+    let p = input.trim().parse::<u32>().expect("请输入整数");
+    input.clear();
+    print!("请输入文法产生式：");
+    std::io::stdout().flush().unwrap();
+    let mut production_vec = Vec::new();
+    for _ in 0..p {
+        // production_vec.push(production.next().unwrap().to_string());
+        // println!("{}", i);
+        std::io::stdin().read_line(&mut input).expect("read error");
+        let mut production = input.trim().split("->");
+        let left = production.next().unwrap().to_string();
+        let right = production.next().unwrap().split("|");
+        let mut right_vec = Vec::new();
+        for r in right {
+            right_vec.push(r.to_string());
+        }
+        production_vec.push((left, right_vec));
+        input.clear();
+    }
+    let grammar = Grammar {
+        terminals: terminal_vec,
+        nonterminals: nonterminal_vec,
+        start: start,
+        rules: production_vec,
+    };
+    return grammar;
+}
+
+
+/// 从文件中读取文法
+pub fn readgrammarfile(filename: &str) -> Grammar {
+    let contents = readfile(filename);
+    let mut lines = contents.split("\n");
+    let m = lines.next().unwrap().trim().parse::<u32>().unwrap();
+    // println!("{}", m);
+    let mut nonterminal = Vec::new();
+    let mut nonterminal_line = lines.next().unwrap().trim().split_whitespace();
+    for _ in 0..m {
+        nonterminal.push(nonterminal_line.next().unwrap().trim().to_string());
+    }
+    let n = lines.next().unwrap().parse::<u32>().unwrap();
+    // println!("{}", n);
+    let mut terminal = Vec::new();
+    let mut terminal_line = lines.next().unwrap().trim().split_whitespace();
+    for _ in 0..n {
+        terminal.push(terminal_line.next().unwrap().trim().to_string());
+    }
+    let start = lines.next().unwrap().trim().to_string();
+    // println!("{}", start);
+    let p = lines.next().unwrap().trim().parse::<u32>().unwrap();
+    // println!("{}", p);
+    let mut production = Vec::new();
+    for _ in 0..p {
+        let mut tmp = lines.next().unwrap().split("->");
+        let left = tmp.next().unwrap().trim().to_string();
+        let right = tmp.next().unwrap().split("|");
+        let mut right_vec = Vec::new();
+        for r in right {
+            right_vec.push(r.trim().to_string());
+        }
+        production.push((left, right_vec));
+    }
+    let grammar = Grammar {
+        terminals: terminal,
+        nonterminals: nonterminal,
+        start: start,
+        rules: production,
+    };
+    return grammar;
+}
